a271e9d15b321a81d498729796cc52f69d670fb0
BenitaMichael <benubiaka@gmail.com>
2024-06-07 17:28:07 +0100

    fix: flowbite configuration

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,22 +1,45 @@
-import { Navbar, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-import logo from '../assets/logo-bg removed.png'
+import { FaMoon } from 'react-icons/fa';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { Button, Navbar, TextInput } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import React from 'react';
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
-    <div>
-      <Navbar className='px-5 py-4 border-b-2'>
-        <Link to="/">
-            <img src={logo} className='self-center max-w-xs class bg-gradient-to-r from-purple-900 via-purple-600 to-pink-500 logo'/>
-        </Link>
-        <form>
-          <TextInput/>
-        </form>
-      </Navbar>
-    </div>
+    <Navbar>
+      <Link
+        to='/'
+        className='flex items-center bg-gradient-to-r from-purple-900 via-purple-600 to-pink-500 rounded-lg whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white p-2'
+      >
+        <img src={logo} alt="Logo" className='h-8 w-8' />
+        <span className='px-2 py-1 text-white'>
+          Dark-Light
+        </span>
+      </Link>
+
+      <form className='flex flex-1 justify-end hidden sm:flex'>
+        <div className='flex items-center border border-gray-300 rounded-lg overflow-hidden'>
+          <TextInput 
+            type='text'
+            placeholder='Search...'
+            className='border-none focus:ring-0'
+          />
+          <button type="submit" className='bg-blue-500 text-white p-2'>
+            <AiOutlineSearch className='w-5 h-5' />
+          </button>
+        </div>
+      </form>
+      
+      <Button
+        className='w-12 h-10 flex items-center justify-center'
+        color='gray'
+        pill
+      >
+        <FaMoon />
+      </Button>
+    </Navbar>
   )
 }
 
-export default Header
+export default Header;
