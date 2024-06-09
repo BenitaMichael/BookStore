87472bfd7d9f74909efc5f5aa8f55e7d86178fa8
BenitaMichael <benubiaka@gmail.com>
2024-06-09 11:07:56 +0100

    feat: create test api route

--- api/index.js
+++ api/index.js
@@ -1,21 +1,24 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
-    .then(() =>{
-        console.log('Mongodb connected')
+    .then(() => {
+        console.log('MongoDB connected');
     })
-    .catch((err) =>{
-        console.log(err)
-    })
+    .catch((err) => {
+        console.log(err);
+    });
 
-const PORT = 3000
+const PORT = 3000;
 
 const app = express();
 
-app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+app.use('/api/user', userRoutes);
