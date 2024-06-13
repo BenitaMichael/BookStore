8003545fdf5b4d2c2b5cc3277cd1f6c0dce13876
BenitaMichael <benubiaka@gmail.com>
2024-06-13 16:42:57 +0100

    feat: signup UI

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png';
+
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='min-h-screen mt-20'>
+      <div className="flex p-4 max-w-3xl mx-auto">
+        {/* left */}
+        <div className="">
+        <Link
+              to='/'
+              className='w-12 m-2 flex items-center bg-gradient-to-r from-purple-900 via-purple-600 to-pink-500 rounded-lg text-4xl font-bold p-2'
+            >
+              <img src={logo} alt="Logo" className='h-10 w-10' />
+              <span className='px-2 py-1 text-black whitespace-nowrap'>
+                Dark-Light
+              </span>
+            </Link>
+            <p className='text-black font-semibold'>Home to wonders and adventures</p>
+          </div>
+
+        {/* right */}
+        <div className=""></div>
+      </div>
+    </div>
   )
 }
 
