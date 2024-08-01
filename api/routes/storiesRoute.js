23efb53050ad6f53e321cbb5fda7b309731abbb3
BenitaMichael <benubiaka@gmail.com>
2024-08-01 22:44:14 +0100

    feat: api route to retrieve stories

--- api/routes/storiesRoute.js
+++ api/routes/storiesRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create } from '../controllers/storiesController.js';
+import { create, getStories } from '../controllers/storiesController.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create);
+router.get('/getstories', getStories)
 
 export default router;
