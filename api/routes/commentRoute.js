289598ce5b8911d1ffedf3e229056b05d846894e
BenitaMichael <benubiaka@gmail.com>
2024-09-28 02:50:41 +0100

    feat: implemented like, edit, and delete functionality

--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createComment, getStoryComments } from '../controllers/commentController.js';
+import {createComment,
+        deleteComment,
+        editComment,
+        getStoryComments,
+        likeComment,} from '../controllers/commentController.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createComment);
-router.get('/getStoryComments/:storyId', getStoryComments)
+router.get('/getStoryComments/:storyId', getStoryComments);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
+router.put('/editComment/:commentId', verifyToken, editComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
 
 export default router;
