b74eec0d9ef833923d2305b889cf7fa8b1d57ca6
Benita Michael <benubiaka@gmail.com>
2025-01-24 03:56:25 +0100

    fix: tailwind configuration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,12 @@
-import flowbite from "flowbite/plugin";
-import tailwindScrollbar from "tailwind-scrollbar";
+const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
-    "./node_modules/flowbite-react/**/*.js",
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+    flowbite.content(),
   ],
   theme: {
     extend: {},
@@ -16,4 +15,5 @@ export default {
     flowbite,
     tailwindScrollbar,
   ],
-};
+}
+
